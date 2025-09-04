d2d2d9d9dbfc79ae08201e9190d6fda41504e52a
Antonio Shilon <shilong_92@hotmail.com>
2025-09-03 18:32:25 -0600

    Feat: ci-cd

--- menu_engineering/settings_test.py
+++ menu_engineering/settings_test.py
@@ -56,7 +56,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pos-menu',
-        'USER': 'pos',
+        'USER': 'postgres',
         'PASSWORD': 'as251292',
         'HOST': 'localhost',
         'PORT': '5432',
